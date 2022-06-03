07a2f88a5b262b05cceb973fac14c0fd17da6ee0
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-03 12:16:06 +0200

    First Commit

---
+++ src/jana60/HelloJava.java
@@ -0,0 +1,13 @@
+package jana60;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Hello Java!");
+		System.out.println("Ciao Biagio!");
+
+	}
+
+}
